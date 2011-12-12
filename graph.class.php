3e13c4ac69f91efef3324693810a8757846b3d2f
corretge <alex@corretge.cat>
2011-12-12 11:02:50 +0100

    previously graph change to show statistics as a table, to copy and paste to Spreadsheet without pain

--- graph.class.php
+++ graph.class.php
@@ -2,221 +2,238 @@
 
 require 'noutrace.class.php';
 
-class graph extends noutrace
+class graph
+  extends noutrace
 {
 
-	protected $aScripts = array();
-	protected $aFunc = array();
-	protected $totTim = 0;
-	protected $totMem = 0;
+  protected $aScripts = array();
+  protected $aFunc = array();
+  protected $totTim = 0;
+  protected $totMem = 0;
+  protected $totInstructions = 0;
   protected $md5File;
 
-	/**
-	 * establim els paràmetres que ens arriben del formulari.
-	 */
-	public function setParams()
-	{
-		$this->file = basename($_GET['file']);
+  /**
+   * establim els paràmetres que ens arriben del formulari.
+   */
+  public function setParams()
+  {
+    $this->file = basename($_GET['file']);
     $this->md5File = md5($this->file);
 
-		/**
-		 * mirem que sigui un arxiu vàlid
-		 */
-		if (!file_exists($this->logDirectory . '/' . $this->file))
-		{
-			throw new Exception("Can't access to file " . $this->logDirectory . '/' . $this->file);
-		}
+    /**
+     * mirem que sigui un arxiu vàlid
+     */
+    if (!file_exists($this->logDirectory . '/' . $this->file))
+    {
+      throw new Exception("Can't access to file " . $this->logDirectory . '/' . $this->file);
+    }
 
-		$this->filesize = filesize($this->logDirectory . '/' . $this->file);
-	}
+    $this->filesize = filesize($this->logDirectory . '/' . $this->file);
+  }
 
-	private function output()
-	{
-		echo "<h2>Total time {$this->totTim} seconds</h2>";
-		echo "<h2>Max memory " . number_format($this->totMem,0)  . " bytes</h2>";
-		echo "<img src=\"plot_centa.php?file={$this->md5File}\" />";
+  private function output()
+  {
+    echo "<h2>Total time {$this->totTim} seconds</h2>";
+    echo "<h2>Max memory " . number_format($this->totMem, 0) . " bytes</h2>";
+    echo "<h2>Total instructions " . number_format($this->totInstructions, 0) . "</h2>";
+    echo "<img src=\"plot_centa.php?file={$this->md5File}\" />";
 
-		$class = 'odd';
-		
-		echo "<h2>Scripts with time > 1 milisecond or memory > 1MB</h2><ul>";
-		foreach ($this->aScripts as $script => $value)
-		{
-			if ($value['tim'] > 0.001 or $value['mem'] > 1000000)
-			{
-				if ($class == 'odd')
-				{
-					$class = 'even';
-				}
-				else
-				{
-					$class = 'odd';
-				}
+    $class = 'odd';
 
-				if ($value['tim'] > $this->totTim/20 or $value['mem'] > $this->totMem/20)
-				{
-						$alarm = 'alarm';
-				}
-				else
-				{
-					$alarm = '';
-				}
-				
-				
-				$value['tim'] = number_format($value['tim'] * 1000000, 0) . ' µs';
-				$value['mem'] = number_format($value['mem'] / 1024, 0) . ' KB';
-				echo "<li class=\"${class} {$alarm}\">";
-				echo "<span class=\"time\">{$value['tim']}</span>";
-				echo "<span class=\"mem\">{$value['mem']}</span>";
-				echo "<span class=\"func\">{$script}</span>";
-				echo "</li>";
-			}
-		}
-		echo "</ul>";
-		
-		
-				echo "<h2>Operations with acumulated time > 1 milisecond or memory > 1MB</h2><ul>";
-		foreach ($this->aFunc as $script => $value)
-		{
-			if ($value['tim'] > 0.001 or $value['mem'] > 1000000)
-			{
-				if ($class == 'odd')
-				{
-					$class = 'even';
-				}
-				else
-				{
-					$class = 'odd';
-				}
-	if ($value['tim'] > $this->totTim/20 or $value['mem'] > $this->totMem/20)
-				{
-						$alarm = 'alarm';
-				}
-				else
-				{
-					$alarm = '';
-				}
-				$value['tim'] = number_format($value['tim'] * 1000000, 0) . ' µs';
-				$value['mem'] = number_format($value['mem'] / 1024, 0) . ' KB';
-				echo "<li class=\"${class} {$alarm}\">";
-				echo "<span class=\"time\">{$value['tim']}</span>";
-				echo "<span class=\"mem\">{$value['mem']}</span>";
-				echo "<span class=\"func\">{$script}</span>";
-				echo "</li>";
-			}
-		}
-		echo "</ul>";
-	}
+    //echo "<h2>Scripts with time > 1 milisecond or memory > 1MB</h2><ul>";
+    echo "<h2>Scripts</h2><ul>";
+    foreach ($this->aScripts as $script => $value)
+    {
+      // if ($value['tim'] > 0.001 or $value['mem'] > 1000000)
+      if (true)
+      {
+        if ($class == 'odd')
+        {
+          $class = 'even';
+        }
+        else
+        {
+          $class = 'odd';
+        }
 
-	public function trace()
-	{
-		/**
-		 * Process all lines
-		 */
-		$fh = fopen($this->logDirectory . '/' . $this->file, 'r');
-		$nRow = 0;
-		$prevTim = 0;
-		$prevCenta = 0;
-		$prevMem = 0;
-		$lastMem = 0;
-
-		$_SESSION['plot_centa'][$this->md5File] = array();
-
-		while ($jReadedLine = fgets($fh))
-		{
-			$nRow++;
+        if ($value['tim'] > $this->totTim / 20 or $value['mem'] > $this->totMem / 20)
+        {
+          $alarm = 'alarm';
+        }
+        else
+        {
+          $alarm = '';
+        }
 
 
-			$jData = explode("\t", $jReadedLine);
-			$jDataCnt = count($jData);
-
-			/**
-			 * només volem els d'entrada
-			 */
-			if ($jDataCnt == 1)
-			{
-				echo "<pre>$jReadedLine</pre>";
-				continue;
-			}
-			elseif ($jDataCnt < 10)
-			{
-				continue;
-			}
+        $value['tim'] = number_format($value['tim'] * 1000000, 0) . ' µs';
+        $value['mem'] = number_format($value['mem'] / 1024, 0) . ' KB';
+        echo "<li class=\"${class} {$alarm}\">";
+        echo "<span class=\"num\">{$value['num']}</span>";
+        echo "<span class=\"time\">{$value['tim']}</span>";
+        echo "<span class=\"mem\">{$value['mem']}</span>";
+        echo "<span class=\"func\">{$script}</span>";
+        echo "</li>";
+      }
+    }
+    echo "</ul>";
 
 
-			/**
-			 * 
-			 * si hi ha canvi de centèssima, li passem l'acumulat de consum de memòria
-			 */
-			$nouCenta = (int) ($jData[3] * 100);
-			
-			if ($nouCenta != $prevCenta)
-			{
-				$latMem = 0;
-				
-				for ($i = $prevCenta; $i < $nouCenta; $i += 1)
-				{
-					$_SESSION['plot_centa'][$this->md5File][] = number_format($jData[4] / (1024 * 1024), 1);
+    echo "<h2>Operations</h2><ul>";
+    //echo "<h2>Operations with acumulated time > 1 milisecond or memory > 1MB</h2><ul>";
+    foreach ($this->aFunc as $script => $value)
+    {
+      // if ($value['tim'] > 0.001 or $value['mem'] > 1000000)
+      if (true)
+      {
+        if ($class == 'odd')
+        {
+          $class = 'even';
+        }
+        else
+        {
+          $class = 'odd';
+        }
+        if ($value['tim'] > $this->totTim / 20 or $value['mem'] > $this->totMem / 20)
+        {
+          $alarm = 'alarm';
+        }
+        else
+        {
+          $alarm = '';
+        }
+        $value['tim'] = number_format($value['tim'] * 1000000, 0) . ' µs';
+        $value['mem'] = number_format($value['mem'] / 1024, 0) . ' KB';
+        echo "<li class=\"${class} {$alarm}\">";
+        echo "<span class=\"num\">{$value['num']}</span>";
+        echo "<span class=\"time\">{$value['tim']}</span>";
+        echo "<span class=\"mem\">{$value['mem']}</span>";
+        echo "<span class=\"func\">{$script}</span>";
+        echo "</li>";
+      }
+    }
+    echo "</ul>";
+  }
 
-				}
-				
-				$prevCenta = (int) ($jData[3] * 100);
-				
-				/**
-				 * si és el primer cop, inicialitzem
-				 */
-				if ($prevTim == 0)
-				{
-					$prevTim = $jData[3];
-					$prevMem = $jData[4];
-				}
-			}
-			else
-			{
-				$lastMem = (int) ($jData[4] / (1024 * 1024));
-			}
+  public function trace()
+  {
+    /**
+     * Process all lines
+     */
+    $fh = fopen($this->logDirectory . '/' . $this->file, 'r');
+    $nRow = 0;
+    $prevTim = 0;
+    $prevCenta = 0;
+    $prevMem = 0;
+    $lastMem = 0;
 
-			/**
-			 * afegim els temps i la memòria al comptador d'scripts
-			 */
-			if (isset($this->aScripts[$jData[8]]))
-			{
-				$this->aScripts[$jData[8]]['tim'] += ($jData[3] - $prevTim);
-				$this->aScripts[$jData[8]]['mem'] += ($jData[4] - $prevMem);
-			}
-			else
-			{
-				$this->aScripts[$jData[8]] = array('tim' => ($jData[3] - $prevTim),
-					'mem' => ($jData[4] - $prevMem));
-			}
+    $_SESSION['plot_centa'][$this->md5File] = array();
 
-
-			/**
-			 * afegim els temps i la memòria al comptador de funcions
-			 */
-			if (isset($this->aFunc[$jData[5]]))
-			{
-				$this->aFunc[$jData[5]]['tim'] += ($jData[3] - $prevTim);
-				$this->aFunc[$jData[5]]['mem'] += ($jData[4] - $prevMem);
-			}
-			else
-			{
-				$this->aFunc[$jData[5]] = array('tim' => ($jData[3] - $prevTim),
-					'mem' => ($jData[4] - $prevMem));
-			}
-
-			$this->totTim = $jData[3];
-			if ($jData[4]>$this->totMem)
-			{
-				$this->totMem = $jData[4];
-			}
-
-			$prevTim = $jData[3];
-			$prevMem = $jData[4];
-		}
-		$_SESSION['plot_centa'][$this->md5File][] = $lastMem;
+    while ($jReadedLine = fgets($fh))
+    {
+      $nRow++;
 
 
-		$this->output();
-	}
+      $jData = explode("\t", $jReadedLine);
+      $jDataCnt = count($jData);
+
+      /**
+       * només volem els d'entrada
+       */
+      if ($jDataCnt == 1)
+      {
+        echo "<pre>$jReadedLine</pre>";
+        continue;
+      }
+      elseif ($jDataCnt < 10)
+      {
+        continue;
+      }
+
+      /**
+       * Afegim un al comptador d'instruccions
+       */
+      $this->totInstructions++;
+
+      /**
+       * 
+       * si hi ha canvi de centèssima, li passem l'acumulat de consum de memòria
+       */
+      $nouCenta = (int) ($jData[3] * 100);
+
+      if ($nouCenta != $prevCenta)
+      {
+        $latMem = 0;
+
+        for ($i = $prevCenta; $i < $nouCenta; $i += 1)
+        {
+          $_SESSION['plot_centa'][$this->md5File][] = number_format($jData[4] / (1024 * 1024),
+                                                                    1);
+        }
+
+        $prevCenta = (int) ($jData[3] * 100);
+
+        /**
+         * si és el primer cop, inicialitzem
+         */
+        if ($prevTim == 0)
+        {
+          $prevTim = $jData[3];
+          $prevMem = $jData[4];
+        }
+      }
+      else
+      {
+        $lastMem = (int) ($jData[4] / (1024 * 1024));
+      }
+
+      /**
+       * afegim els temps i la memòria al comptador d'scripts
+       */
+      if (isset($this->aScripts[$jData[8]]))
+      {
+        $this->aScripts[$jData[8]]['tim'] += ($jData[3] - $prevTim);
+        $this->aScripts[$jData[8]]['mem'] += ($jData[4] - $prevMem);
+        $this->aScripts[$jData[8]]['num']++;
+      }
+      else
+      {
+        $this->aScripts[$jData[8]] = array('tim' => ($jData[3] - $prevTim),
+          'mem' => ($jData[4] - $prevMem),
+          'num' => 1);
+      }
+
+
+      /**
+       * afegim els temps i la memòria al comptador de funcions
+       */
+      if (isset($this->aFunc[$jData[5]]))
+      {
+        $this->aFunc[$jData[5]]['tim'] += ($jData[3] - $prevTim);
+        $this->aFunc[$jData[5]]['mem'] += ($jData[4] - $prevMem);
+        $this->aFunc[$jData[5]]['num']++;
+      }
+      else
+      {
+        $this->aFunc[$jData[5]] = array('tim' => ($jData[3] - $prevTim),
+          'mem' => ($jData[4] - $prevMem),
+          'num' => 1);
+      }
+
+      $this->totTim = $jData[3];
+      if ($jData[4] > $this->totMem)
+      {
+        $this->totMem = $jData[4];
+      }
+
+      $prevTim = $jData[3];
+      $prevMem = $jData[4];
+    }
+    $_SESSION['plot_centa'][$this->md5File][] = $lastMem;
+
+
+    $this->output();
+  }
 
 }
